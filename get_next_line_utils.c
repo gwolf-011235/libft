531a15432e0339b01a85a464e545c77d8c153d65
Gabriel <gwolf@student.42vienna.com>
2023-01-05 11:18:22 +0100

    fix(get_next_line_utils): deleted duplicate functions

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,40 +6,11 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 11:20:13 by gwolf             #+#    #+#             */
-/*   Updated: 2022/12/02 09:12:53 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/01/05 11:16:31 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	*ft_memchr(const void *s, int c, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (n--)
-	{
-		if (((unsigned char *)s)[i] == (unsigned char) c)
-			return ((void *)s + i);
-		i++;
-	}
-	return (NULL);
-}
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	size_t			i;
-
-	if (dest == NULL && src == NULL)
-		return (dest);
-	i = 0;
-	while (n--)
-	{
-		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-		i++;
-	}
-	return (dest);
-}
 
 char	*ft_lstclear_plus(t_buf_node **head, bool fclear)
 {
