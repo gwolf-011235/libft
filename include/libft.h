6a2966771e0160a516024dfcc24c9d0d8916df7a
Gabriel Wolf <gwolf@student.42vienna.com>
2023-11-11 10:34:56 +0100

    feat: new combining libft.h

---
+++ include/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 10:33:02 by gwolf             #+#    #+#             */
+/*   Updated: 2023/11/11 10:34:30 by gwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBT_H
+
+# include "ft_char.h"
+# include "ft_fd.h"
+# include "ft_lst.h"
+# include "ft_mem.h"
+# include "ft_num.h"
+# include "ft_print.h"
+# include "ft_str.h"
+
+#endif
